985eb8c9ad5e0390104605212de91d65d0d5c304
alla2001 <48186577+alla2001@users.noreply.github.com>
2023-01-26 20:01:10 +0100

    Update main.ts

--- src/main/main.ts
+++ src/main/main.ts
@@ -14,7 +14,7 @@ import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
-
+//
 class AppUpdater {
   constructor() {
     log.transports.file.level = 'info';
