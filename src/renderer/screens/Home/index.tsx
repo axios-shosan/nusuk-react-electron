111fae4dec0a6f1c506002325446d4007e9513d4
axios <medmohamed923@gmail.com>
2023-01-30 03:07:14 +0100

    Finished Home

--- src/renderer/screens/Home/index.tsx
+++ src/renderer/screens/Home/index.tsx
@@ -1,11 +1,28 @@
-import Hero from 'renderer/components/Hero';
-import Img from '../../../../assets/images/home/home-background.png';
+import Button from 'renderer/components/Button';
+import { useNavigate } from 'react-router-dom';
+import Logo from '../../../../assets/logo.svg';
+import wavesBg from '../../../../assets/images/hero-wave-dark.png';
+import Classes from './style.module.css';
 
 export default function Home() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <a href="/">H</a>
-      <Hero image={Img} animation="expand" />;
+    <div className={Classes.container}>
+      <img src={wavesBg} alt="Waves Bg" className={Classes.wavesBg} />
+      <div className={`${Classes.waves} ${Classes.expandedWaves}`} />
+      <img src={Logo} alt="Logo" className={Classes.logo} />
+      <h1 className={Classes.title}>
+        Welcome to Nusuk your official guide to Makkah and Madina
+      </h1>
+      <Button
+        extraClasses={`${Classes.homeBtn}`}
+        onClick={() => {
+          navigate('/');
+        }}
+        active
+      >
+        <p className={Classes.btnText}>Explore</p>
+      </Button>
     </div>
   );
 }
