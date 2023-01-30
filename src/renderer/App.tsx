904ba389570b70991500ba4957c47ee21d4da6b2
Mehdi BENNOUNA <mehdi.m.bennouna@gmail.com>
2023-01-30 13:02:48 +0100

    fast-forward azpect to axios

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -11,6 +11,33 @@ export default function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+
+          <Route path="umrah">
+            <Route path="about" />
+            <Route path="pillars" />
+            <Route path="miqat" />
+            <Route path="mosque" />
+            <Route path="tips" />
+            <Route path="accessibility" />
+            <Route path="ziyarah" />
+          </Route>
+
+          <Route path="makkah">
+            <Route path="accomodation" />
+            <Route path="holy-sites" />
+            <Route path="landmarks" />
+            <Route path="shopping" />
+            <Route path="food-drinks" />
+            <Route path="to-makkah" />
+          </Route>
+
+          <Route path="madina">
+            <Route path="accomodation" />
+            <Route path="landmarks" />
+            <Route path="shopping" />
+            <Route path="food-drinks" />
+            <Route path="to-madina" />
+          </Route>
           <Route path="/makkah" element={<Makkah />} />
         </Routes>
       </Router>
