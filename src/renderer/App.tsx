ebe6fda3622cb5a5c2f4a91ee40b0b3fd46c290d
axios <medmohamed923@gmail.com>
2023-02-01 04:29:05 +0100

    fix

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -34,7 +34,6 @@ export default function App() {
   return (
     <>
       <Router>
-        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="explore" element={<Explore />} />
--- src/renderer/screens/Home/index.tsx
+++ src/renderer/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import Button from 'renderer/components/Button';
 import { useNavigate } from 'react-router-dom';
 import Logo from '../../../../assets/logo.svg';
 import wavesBg from '../../../../assets/images/hero-wave-dark.png';
+import waves from '../../../../assets/images/Waves.png';
 import Classes from './style.module.css';
 
 export default function Home() {
@@ -12,7 +13,11 @@ export default function Home() {
     <>
       <div className={Classes.container}>
         <img src={wavesBg} alt="Waves Bg" className={Classes.wavesBg} />
-        <div className={`${Classes.waves}`} />
+        <img
+          src={waves}
+          alt="Waves"
+          className={`${Classes.waves} ${Classes.maskedWaves}`}
+        />
         <img src={Logo} alt="Logo" className={Classes.logo} />
         <h1 className={Classes.title}>
           Welcome to Nusuk your official guide to Makkah and Madina
